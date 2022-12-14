e930beffcc3c38893adbd4ebbcd8af1b42a8c002
sarah <sbellemare99@gmail.com>
2022-12-14 18:42:24 -0500

    right

--- Board.py
+++ Board.py
@@ -16,6 +16,14 @@ class Board:
             cells.append(columns)
         return cells
 
+    def setCell(self, column, row, value):
+        cell = self.cells[column][row]
+        if cell.value == -1:
+            cell.fill(value)
+            return True
+        else:
+            return False
+
     def printCells(self):
         values = []
         for i in range(self.size):
@@ -25,3 +33,41 @@ class Board:
             values.append(rowValues)
         for i in values:
             print(i)
+
+    def moveCells(self, moves):
+        for move in moves:
+            if move == 0:
+                self.left()
+            elif move == 1:
+                self.down()
+            elif move == 2:
+                self.up()
+            elif move == 3:
+                self.right()
+            self.printCells()
+
+    def left(self):
+        possible = False
+        for i in range(self.size):
+            for j in reversed(range(self.size)):
+                ##print(self.cells[i][j].value)
+                ##print(self.cells[i][j-1].value)
+                if self.cells[i][j].value == self.cells[i][j - 1].value and self.cells[i][j].value != -1:
+                    self.cells[i][j - 1].fill(self.cells[i][j].value * 2)
+                    self.cells[i][j].empty()
+                    possible = True
+                elif self.cells[i][j - 1].value == -1 and self.cells[i][j].value != -1:
+                    self.cells[i][j - 1].fill(self.cells[i][j].value)
+                    self.cells[i][j].empty()
+                    possible = True
+        if possible == False:
+            print("this move won't do anything")
+
+    def down(self):
+        pass
+
+    def up(self):
+        pass
+
+    def right(self):
+        pass
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # This is a sample Python script.
+import random
+
 from Board import Board
 
 
@@ -6,14 +8,40 @@ from Board import Board
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+size = 2
+moves = [0, 0, 0]
+def randomNumber():
+    column = random.randint(0, size-1)
+    row = random.randint(0, size-1)
+    value = 0
+    generator = random.randint(0,1)
+    if generator == 0:
+        value = 2
+    elif generator == 1:
+        value = 4
+    return column, row, value
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    board = Board(2)
+    board = Board(size)
     board.printCells()
+    column1, row1, value1 = randomNumber()
+    column2, row2, value2 = randomNumber()
+    filleable = board.setCell(column1, row1, value1)
+    filleable2 = board.setCell(column2, row2, value2)
+    print("Filling the board on first try")
+    while(filleable2 == False):
+        column2, row2, value2 = randomNumber()
+        filleable2 = board.setCell(column2, row2, value2)
+    board.printCells()
+    print("Starting game")
+    board.moveCells(moves)
+
+
+
+
+
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
