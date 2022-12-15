dd72818040afb6b56c5fe3b0141033203a106583
sarah <sbellemare99@gmail.com>
2022-12-14 19:40:22 -0500

    done game

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Board import Board
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-size = 4
+size = 2
 moves = [0, 2, 3, 6, 1, 3, 2, 2, 1, 2, 1, 0, 3, 2, 2, 1, 0, 1, 0, 2, 3, 3, 2]
 def randomNumber():
     column = random.randint(0, size-1)
